38fa1613116e6d3f7f07096d3a96b85774f27a88
Sophia <sophia28.soares@gmail.com>
2026-04-22 23:45:15 +0000

    mula 1.0

--- script.js
+++ script.js
@@ -27,67 +27,81 @@ btn.onclick = () => {
   saldo -= aposta;
   atualizarSaldo();
 
-  somGiro.currentTime = 0;
   somGiro.play();
 
-  girarRolo(r1, 10, 80, () => {
-    girarRolo(r2, 15, 80, () => {
-      girarRolo(r3, 20, 80, resultadoFinal);
+  girarRolo(r1, 10, () => {
+    girarRolo(r2, 15, () => {
+      girarRolo(r3, 20, resultadoFinal);
     });
   });
 
   function resultadoFinal() {
     let res = [r1.innerText, r2.innerText, r3.innerText];
-
     let texto = "";
 
-    // 🎉 JACKPOT
     if (res[0] === res[1] && res[1] === res[2]) {
       let ganho = aposta * 5;
 
-      // 🪙 bônus especial 🍒
       if (res[0] === "🍒") {
         ganho *= 2;
-        texto = "🍒 SUPER BÔNUS!!! ";
+        texto = "🍒 SUPER BÔNUS! ";
       }
 
       saldo += ganho;
       somWin.play();
+      efeitoChuva("💰", 40);
+
       texto += "🎉 JACKPOT! +" + ganho.toFixed(2);
 
     } else if (new Set(res).size === 2) {
       let ganho = aposta * 2;
       saldo += ganho;
+      efeitoChuva("💰", 20);
+
       texto = "✨ Dois iguais! +" + ganho.toFixed(2);
 
     } else {
-      texto = "❌ Perdeu!";
+      efeitoChuva("🐴", 50);
+      texto = "🐴 Você foi mulado!";
     }
 
     msg.innerText = texto;
     atualizarSaldo();
-    salvar();
+    localStorage.setItem("saldo", saldo);
   }
 };
 
-function girarRolo(elemento, vezes, tempo, callback) {
+function girarRolo(el, vezes, callback) {
   let i = 0;
-
   let intervalo = setInterval(() => {
-    elemento.innerText = simbolos[Math.floor(Math.random() * simbolos.length)];
+    el.innerText = simbolos[Math.floor(Math.random() * simbolos.length)];
     i++;
 
     if (i >= vezes) {
       clearInterval(intervalo);
       if (callback) callback();
     }
-  }, tempo);
+  }, 80);
 }
 
 function atualizarSaldo() {
   document.getElementById("saldo").innerText = saldo.toFixed(2);
 }
 
-function salvar() {
-  localStorage.setItem("saldo", saldo);
+/* EFEITOS */
+function efeitoChuva(emoji, quantidade) {
+  const container = document.getElementById("efeitos");
+
+  for (let i = 0; i < quantidade; i++) {
+    const el = document.createElement("div");
+    el.classList.add("item");
+    el.innerText = emoji;
+
+    el.style.left = Math.random() * 100 + "vw";
+    el.style.animationDuration = (Math.random() * 2 + 2) + "s";
+
+    container.appendChild(el);
+
+    setTimeout(() => el.remove(), 4000);
+  }
 }
